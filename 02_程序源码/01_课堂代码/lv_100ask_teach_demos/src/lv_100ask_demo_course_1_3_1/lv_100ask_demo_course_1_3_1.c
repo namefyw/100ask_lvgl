95f24cefec9c250a956e361564b708c2a4b16220
zhouyuebiao <837574599@qq.com>
2021-12-08 19:05:55 +0800

    first commit

---
+++ 02_程序源码/01_课堂代码/lv_100ask_teach_demos/src/lv_100ask_demo_course_1_3_1/lv_100ask_demo_course_1_3_1.c
@@ -0,0 +1,50 @@
+/**
+ ******************************************************************************
+ * @file    lv_100ask_demo_course_1_3_1.c
+ * @author  百问科技
+ * @version V1.0
+ * @date    2021-12-02
+ * @brief	1-3-1课的课堂代码
+ ******************************************************************************
+ * Change Logs:
+ * Date           Author          Notes
+ * 2021-12-02     zhouyuebiao     First version
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (C) 2008-2021 深圳百问网科技有限公司<https://www.100ask.net/>
+ * All rights reserved
+ *
+ ******************************************************************************
+ */
+
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../../lv_100ask_teach_demos.h"
+
+#if LV_USE_100ASK_DEMO_COURSE_1_3_1
+
+#include <stdio.h>
+#include "lv_100ask_demo_course_1_3_1.h"
+
+
+/*********************
+ *      DEFINES
+ *********************/
+
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+
+void lv_100ask_demo_course_1_3_1(void)
+{
+    lv_obj_t * obj = lv_obj_create(lv_scr_act());
+    lv_obj_center(obj);
+}
+
+
+#endif /* LV_USE_100ASK_DEMO_COURSE_1_3_1 */
