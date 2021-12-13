94ca2d43a3d73550856e8cc873fbc8c7b0b4b60e
100ask <100ask@100ask.com>
2021-12-13 17:43:39 +0800

    更新lv_100ask_teach_demos

--- 02_程序源码/02_项目模板/00_lv_100ask_sim_codeblocks_win/main.c
+++ 02_程序源码/02_项目模板/00_lv_100ask_sim_codeblocks_win/main.c
@@ -51,7 +51,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLi
     lv_win32_init(hInstance, SW_SHOWNORMAL, 1024, 600, NULL);
 
     /*Run the demo*/
-    lv_100ask_demo_course_1_2_1();
+    lv_100ask_demo_course_1_3_1();
 
     while(!lv_win32_quit_signal) {
         /* Periodically call the lv_task handler.
--- 02_程序源码/02_项目模板/01_lv_100ask_port_stm32f103_tft_320x480/lv_100ask_teach_demos/src/lv_100ask_demo_course_1_3_1/lv_100ask_demo_course_1_3_1.h
+++ 02_程序源码/02_项目模板/01_lv_100ask_port_stm32f103_tft_320x480/lv_100ask_teach_demos/src/lv_100ask_demo_course_1_3_1/lv_100ask_demo_course_1_3_1.h
@@ -1,25 +1,21 @@
-/**
- * @file lv_100ask_teach_demos.h
- * This file exists only to be compatible with Arduino's library structure
- */
-
-#ifndef LV_100ASK_TEACH_DEMOS_SRC_H
-#define LV_100ASK_TEACH_DEMOS_SRC_H
+#ifndef LV_100ASK_DEMO_COURSE_1_3_1_H
+#define LV_100ASK_DEMO_COURSE_1_3_1_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
 /*********************
  *      INCLUDES
  *********************/
-
 #include "../lv_100ask_teach_demos.h"
+
 
 /*********************
  *      DEFINES
  *********************/
- 
+
 /**********************
  *      TYPEDEFS
  **********************/
@@ -27,14 +23,21 @@ extern "C" {
 /**********************
  * GLOBAL PROTOTYPES
  **********************/
+void lv_100ask_demo_course_1_3_1(void);
+
+
 
 /**********************
  *      MACROS
  **********************/
 
-
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
 
-#endif /*LV_100ASK_TEACH_DEMOS_SRC_H*/
+#endif /* LV_100ASK_DEMO_COURSE_1_3_1_H */
+
+
+
+
+
