16cfc6c0d2155d829600bd53027a12935515112f
YobeZhou <smilezyb@163.com>
2023-08-22 17:49:03 +0800

    fix(课堂示例代码): 更新示例：lv_100ask_demo_course_2_1_1

--- 02_程序源码/02_项目模板/00_lv_100ask_sim_codeblocks_win/lv_100ask_teach_demos/src/lv_100ask_demo_course_2_1_1/lv_100ask_demo_course_2_1_1.c
+++ 02_程序源码/02_项目模板/00_lv_100ask_sim_codeblocks_win/lv_100ask_teach_demos/src/lv_100ask_demo_course_2_1_1/lv_100ask_demo_course_2_1_1.c
@@ -42,11 +42,12 @@
 void lv_100ask_demo_course_2_1_1(void)
 {
     lv_obj_t * obj = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(obj, LV_PCT(20), LV_PCT(10));
+    //lv_obj_set_size(obj, LV_PCT(20), LV_PCT(10));
+    lv_obj_set_size(obj, LV_PCT(40), LV_PCT(20));
     lv_obj_align(obj, LV_ALIGN_CENTER, 0, 0);
 
     lv_obj_t * label = lv_label_create(obj);
-    lv_label_set_text(label, "Hello, LVGL!");
+    lv_label_set_text_fmt(label, "Hello, LVGL!\nLVGL V%d.%d.%d\nhttps://www.100ask.net", lv_version_major(), lv_version_minor(), lv_version_patch());
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
 
